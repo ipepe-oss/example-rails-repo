fa01f7ea0ce0f321126641960a9648826b3b779e
Patryk Ptasinski <patryk@ipepe.pl>
2024-03-14 12:52:33 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/", to: lambda { |_env|
-    [200, { "Content-Type" => "text/html" }, ["Hello, world!"]]
-  }
+  get "/", to: ->(_env) { [200, { "Content-Type" => "text/html" }, ["Hello, world!"]] }
 
   # Defines the root path route ("/")
   # root "posts#index"
